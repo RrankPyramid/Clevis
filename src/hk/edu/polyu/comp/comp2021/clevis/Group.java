df70b79cabd5f682bd5081294b10f403d6b55316
Liu-KM <73509271+Liu-KM@users.noreply.github.com>
2021-11-02 16:47:51 +0800

    Add Group.java

---
+++ src/hk/edu/polyu/comp/comp2021/clevis/Group.java
@@ -0,0 +1,21 @@
+package hk.edu.polyu.comp.comp2021.clevis;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Group extends Shape{
+    private ArrayList<Shape> list;
+    public Group(){
+        list = new ArrayList<>();
+    }
+    public void add_Shape(Shape s){
+        list.add(s);
+    }
+    @Override
+    public void draw(Graphics g){
+        for (Shape s : this.list){
+            s.draw(g);
+        }
+    }
+
+}
