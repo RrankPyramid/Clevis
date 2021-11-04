2ee646b5bf4bc98f53469f1be5aba09046cf6e9a
kumaxiong <1595549405@qq.com>
2021-11-04 19:53:16 +0800

    remake main

--- src/hk/edu/polyu/comp/comp2021/clevis/Application.java
+++ src/hk/edu/polyu/comp/comp2021/clevis/Application.java
@@ -1,19 +1,18 @@
 package hk.edu.polyu.comp.comp2021.clevis;
 
 import hk.edu.polyu.comp.comp2021.clevis.model.Clevis;
+import hk.edu.polyu.comp.comp2021.clevis.util.Vector;
+import hk.edu.polyu.comp.comp2021.clevis.util.Vertex;
 
 public class Application {
 
     public static void main(String[] args){
         Clevis clevis = new Clevis();
         // Initialize and utilize the system
-        Picture pic = new Picture(2000,2000);
-//       Circle c1 = new Circle(200,200,200);
-//       Rectangle r1 = new Rectangle(0, 0, 100, 100);
-//        Line l1 = new Line(0,205,400,205);
+        Picture pic = new Picture(600,600);
+       Rectangle r1 = new Rectangle(new Vertex(20,20),new Vector(100,200));
 //        Circle c2 = new Circle(200,200,100);
-//        pic.add(c1);
-//        pic.add(r1);
+        pic.add(r1);
 //        pic.add(l1);
 //        pic.add(c2);
         pic.draw();
