9f5276e4cf08d402ac56a6caaa3245a1c10a397b
Liu-KM <73509271+Liu-KM@users.noreply.github.com>
2021-11-16 01:07:06 +0800

    CLI finish

--- src/hk/edu/polyu/comp/comp2021/clevis/util/GraphConstant.java
+++ src/hk/edu/polyu/comp/comp2021/clevis/util/GraphConstant.java
@@ -15,4 +15,13 @@ public final class GraphConstant {
      * max number in this app
      */
     public static final double INF = 2000;
+    /**
+     * The width of the page
+     */
+    public static final int WIDTH = 1000;
+    /**
+     * The height of the page
+     */
+    public static final int HEIGHT = 1000;
+
 }
