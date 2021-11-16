b8c67223f0d0c0869a0c27016e4f74e4290e476b
Liu-KM <73509271+Liu-KM@users.noreply.github.com>
2021-11-16 11:41:57 +0800

    Add part of test

---
+++ test/hk/edu/polyu/comp/comp2021/clevis/ApplicationTest.java
@@ -0,0 +1,7 @@
+package hk.edu.polyu.comp.comp2021.clevis;
+
+import junit.framework.TestCase;
+
+public class ApplicationTest extends TestCase {
+
+}
--- test/hk/edu/polyu/comp/comp2021/clevis/util/VertexTest.java
+++ test/hk/edu/polyu/comp/comp2021/clevis/util/VertexTest.java
@@ -1,14 +1,33 @@
 package hk.edu.polyu.comp.comp2021.clevis.util;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class VertexTest{
+    Vertex A = null;
+    Vertex B = null;
+    Vertex C = null;
+
+    double x1 = 5;
+    double y1 = 6;
+    double x2 = 7;
+    double y2 = 8;
+    double x3 = 9;
+    double y3 = 10;
+
+    @Before
+    public void before(){
+        A.setX(x1);
+        B.setX(x2);
+        C.setX(x3);
+    }
+
     @Test
     public void testVertexAdd(){
-        Vertex a = new Vertex(1,1);
-        Vector b = new Vector(2, 3);
-        a.add(b);
-        assert true;
+    }
+    @Test
+    public void vectorToTest(){
+
     }
 
 }
